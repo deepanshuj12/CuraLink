2fa155ca04d31cac80a05cf719ef2f367811f0ed
deepanshuj12 <ddeepanshuj29@gmail.com>
2026-04-19 14:38:10 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+const API_BASE = import.meta.env.VITE_API_BASE;
 
 function App() {
   const [token, setToken] = useState(localStorage.getItem("token") || "");
